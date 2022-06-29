07de358b9f7cb56bf130fd717e4e2fc13aae2c0c
M_0v0_M <lightblueskyline@gmail.com>
2022-06-29 11:16:15 +0800

    TodoApi

---
+++ TodoApi/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        // ! = from Nullable to Non-Nullable
+        // ? = from Non-Nullable to Nullable
+        public string Name { get; set; } = null!; // 不允许为空值
+        public decimal Price { get; set; } = 0;
+    }
+}
