ecdf2d4db1d4b3c49677df8be8f9eddef19fbde5
M_0v0_M <lightblueskyline@gmail.com>
2022-07-01 11:31:18 +0800

    共用工具-进制转换

---
+++ UtilityConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UtilityConsole;
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region MyRegion
+        Console.WriteLine(String.Format("{0:X4}", 2000));
+        Console.WriteLine(0x07E4.ToString("N"));
+        #endregion
+    }
+}
